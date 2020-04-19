e061cee7dbd53c3f4a10e688835fc8ebef70097b
cckings <1504468447@qq.com>
2020-04-19 23:38:54 +0800

    处理跨域

--- shopping-wx-back/app.js
+++ shopping-wx-back/app.js
@@ -9,12 +9,24 @@ require("./common/connect-database")
 const router = require('./routes/index')
 var baseCommon = require('./common/common');
 
+/* 跨域设置 */
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
+
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
