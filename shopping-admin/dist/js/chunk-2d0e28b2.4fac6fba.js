ed606920de6f0fb117b182ded0f3c0637eaeb5e0
cckings <1504468447@qq.com>
2020-05-04 17:03:45 +0800

    update gitignore

--- shopping-admin/dist/js/chunk-2d0e28b2.4fac6fba.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e28b2"],{"7ec1":function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div",[e._v(" 404 ")])},a=[],u={name:"index"},i=u,r=c("2877"),s=Object(r["a"])(i,t,a,!1,null,"7bcfafa0",null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0e28b2.4fac6fba.js.map
