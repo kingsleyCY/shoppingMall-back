b8ab0cf24cc95775fdf86154924d46c5892c0778
cckings <1504468447@qq.com>
2020-06-19 23:14:29 +0800

    修改订单状态返回数据

--- shopping-wx-back/routes/wx/order.js
+++ shopping-wx-back/routes/wx/order.js
@@ -317,7 +317,7 @@ router.post("/sureReceipt", async (ctx) => {
     userId: param.userId,
     out_trade_no: param.out_trade_no
   })
-  if (orderItem && orderItem.orderStatus === "deliver") {
+  if (orderItem && orderItem.orderStatus === "delivered") {
     var userItem = await userModel.findOne({ userId: param.userId });
     var integral = (userItem.integral || 0) + (orderItem.total_fee / 100);
     await userModel.findOneAndUpdate({ userId: orderItem.userId }, { integral }, { new: true })
